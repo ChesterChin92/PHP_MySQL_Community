c42b661f97c729db9ce86f7857672b6a83a78a02
Chester Chin <chinschian@hotmail.com>
2018-02-06 09:17:32 +0800

    Added source codes for day 1.

---
+++ DAY1/index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        I need coffee.
+        <b>I need more coffee</b>
+
+        <?php
+            $name = "ShuWei";
+            $a = 2;
+            $b = 1;
+
+            if ($name == "Chester"){
+                echo "<b>My name is ". $name."</b>";
+            }
+            else {
+                echo "I have no idea who this is." ;
+                echo "But the name is". $name;
+            }
+
+
+           
+            echo "<h1>The answer is... </h1>";
+            echo "<button>hello</button>";
+        ?>
+    
+
+        <i>I need much more coffee</i>
+    </body>
+
+</html>  
+ 
